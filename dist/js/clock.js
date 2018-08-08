9989c712d7a8fe5a8ada239b296b8239df926c0e
Karolina Siwkowska <trallala00@gmail.com>
2018-08-08 07:35:20 +0200

    Add zeros to clock,change letter spacing

--- dist/js/clock.js
+++ dist/js/clock.js
@@ -7,8 +7,11 @@ function count() {
     let year = today.getFullYear();
 
     let hour = today.getHours();
+    if (hour < 10) hour = "0" + hour;
     let min = today.getMinutes();
+    if (min < 10) min = "0" + min;
     let sec = today.getSeconds();
+    if (sec < 10) sec = "0" + sec;
 
     document.querySelector(".clock").innerHTML = day + "." + month + "." + year + "|" + hour + ":" + min + ":" + sec;
 
