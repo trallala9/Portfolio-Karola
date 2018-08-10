3175501948becaf5b44d03afb406af139cbb5de5
Karolina Siwkowska <trallala00@gmail.com>
2018-08-10 09:12:58 +0200

    Add zeros to the html date, add links in work.html,add styles

--- dist/js/clock.js
+++ dist/js/clock.js
@@ -3,8 +3,11 @@ function count() {
     let today = new Date();
 
     let day = today.getDate();
+    if (day < 10) day = "0" + day;
     let month = today.getMonth() + 1;
+    if (month < 10) month = "0" + month;
     let year = today.getFullYear();
+    if (year < 10) year = "0" + year;
 
     let hour = today.getHours();
     if (hour < 10) hour = "0" + hour;
